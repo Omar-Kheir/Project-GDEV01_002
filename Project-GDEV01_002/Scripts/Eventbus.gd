e1e5c368bebc0a142c03cfc64cfb1388bcd5d03c
Omar EL Kheir <omarkheir.dev@gmail.com>
2025-02-09 22:42:29 +0200

    Update 2

--- Project-GDEV01_002/Scripts/Eventbus.gd
+++ Project-GDEV01_002/Scripts/Eventbus.gd
@@ -4,9 +4,20 @@ signal gameOver
 signal wallHit
 signal levelPassed
 
+var levels = ["res://Scenes/level_1.tscn", "res://Scenes/level_2.tscn", "res://Scenes/level_3.tscn"]
+var currentLevel = 0
+
 func _ready() -> void:
 	gameOver.connect(endRun)
+	levelPassed.connect(nextLevel)
 	
 func endRun():
 	get_tree().change_scene_to_file.bind("res://Scenes/EndRunMenu.tscn").call_deferred()
 	PlayerStats.reset()
+
+func nextLevel():
+	if currentLevel != 2:
+		currentLevel += 1
+		get_tree().change_scene_to_file.bind(levels[currentLevel]).call_deferred()
+	else:
+		get_tree().change_scene_to_file.bind("res://Scenes/MainMenu.tscn").call_deferred()
